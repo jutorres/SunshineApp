f88feecb6794888334130c231ff2b765b3ee268c
jutorres <torresjujuliana@gmail.com>
2017-06-18 18:01:00 -0300

    First version.

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -8,7 +8,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -50,14 +53,38 @@ public class MainActivity extends ActionBarActivity {
      */
     public static class PlaceholderFragment extends Fragment {
 
+        ArrayAdapter<String> mForecastAdapter;
+
         public PlaceholderFragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
+
+            ArrayList <String> weekForecast = new ArrayList<>();
+            weekForecast.add("Mon 6/23 - Sunny - 31/17");
+            weekForecast.add("Tue 6/24 - Foggy - 21/8");
+            weekForecast.add("Wed 6/25 - Cloudy - 22/17");
+            weekForecast.add("Thurs 6/26 - Rainy - 18/11");
+            weekForecast.add("Fri 6/27 - Foggy - 21/10");
+            weekForecast.add("Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18");
+            weekForecast.add("Sun 6/29 - Sunny - 20/7");
+
+            mForecastAdapter =
+                    new ArrayAdapter<>(
+                        getActivity(),
+                        R.layout.list_item_forecast,
+                        R.id.listView_forecast,
+                        weekForecast);
+
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            ListView listView = (ListView) rootView.findViewById(R.id.listView_forecast);
+            listView.setAdapter(mForecastAdapter);
+
             return rootView;
+
         }
     }
 }
